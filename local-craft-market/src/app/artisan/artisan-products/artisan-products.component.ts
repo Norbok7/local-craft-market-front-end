c77c4d2bda3b0971505a0657b6c78d7bf5fca99e
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-02-13 13:01:34 -0600

    Front End project setup/components/services/module

---
+++ local-craft-market/src/app/artisan/artisan-products/artisan-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisan-products.component.html',
+  styleUrl: './artisan-products.component.css'
+})
+export class ArtisanProductsComponent {
+
+}
---
+++ local-craft-market/src/app/artisan/artisan-profile/artisan-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisan-profile.component.html',
+  styleUrl: './artisan-profile.component.css'
+})
+export class ArtisanProfileComponent {
+
+}
